0738213a8e88abb2c8fb8a058e689e9e8f70c112
Guilherme Martins <g.santosmartins@outlook.com>
2022-05-21 16:39:25 -0300

    feat: Adding the first page navigation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
 // Internal imports
@@ -8,13 +9,19 @@ import useProdutores from './src/hooks/useProdutores';
 import Home from './src/telas/Home';
 import MelhoresProdutores from './src/telas/MelhoresProdutores';
 
+// importin
+const Tab = createBottomTabNavigator();
+
 export default function App() {
 
   return <SafeAreaView style={{ flex: 1 }}>
     <StatusBar />
 
     <NavigationContainer>
-      <Home />
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Melhores" component={MelhoresProdutores} />
+      </Tab.Navigator>
     </NavigationContainer>
 
   </SafeAreaView>
