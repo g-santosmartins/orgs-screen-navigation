e23a451b25376630e9e11a763aeceee704c5fec8
Guilherme Martins <g.santosmartins@outlook.com>
2022-05-21 13:28:01 -0300

    fix: reafactoring the project into simple pages

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
-import useProdutores from './src/hooks/useProdutores';
-
-import Home from './src/telas/Home';
-import Cesta from './src/telas/Cesta';
 import { NavigationContainer } from '@react-navigation/native';
 
+
+// Internal imports
+import useProdutores from './src/hooks/useProdutores';
+import Home from './src/telas/Home';
+import MelhoresProdutores from './src/telas/MelhoresProdutores';
+
 export default function App() {
-  const produtores = useProdutores(false);
 
   return <SafeAreaView style={{ flex: 1 }}>
     <StatusBar />
 
     <NavigationContainer>
-      <Home melhoresProdutores={true} />
-      {/* {produtores.length > 0 && 
-      <Cesta produtor={{
-        nome: produtores[0].nome, 
-        imagem: produtores[0].imagem
-      }}
-      {...produtores[0].cestas[0]} />
-    } */}
+      <Home />
     </NavigationContainer>
 
   </SafeAreaView>
