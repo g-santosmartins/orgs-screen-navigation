c58c238a43251f73538e1bbd54618f86a3c85c95
Guilherme Martins <g.santosmartins@outlook.com>
2022-05-21 21:36:23 -0300

    fix/feat: Improving icons on navigation

--- src/rotas/AppRotas.js
+++ src/rotas/AppRotas.js
@@ -7,13 +7,32 @@ import ProdutorRotas from './ProdutorRotas';
 import MelhoresProdutoresRotas from './MelhoresProdutoresRotas';
 
 
+// icons imports
+
+import Coracao from '../assets/coracao.svg'
+import Home from '../assets/home.svg'
+
+
 const Tab = createBottomTabNavigator();
 
 export default function AppRotas() {
     return(
         <NavigationContainer>
             {/* removes the header -> headerShown === false */}
-            <Tab.Navigator screenOptions={{headerShown: false}}> 
+            <Tab.Navigator screenOptions={({route}) => ({
+                headerShown: false,
+                tabBarIcon: ({color})=> {
+                    let Icon = Home
+                    if(route.name === 'Melhores Produtores') {
+                        Icon = Coracao
+                    }
+
+                    return <Icon color={color}/>
+                },
+                tabBarActiveTintColor: '#2a9f85',
+                tabBarInactiveTintColor: '#c7c7c7',
+                
+                })}> 
                 <Tab.Screen name="Início" component={ProdutorRotas} />
                 <Tab.Screen name="Melhores Produtores" component={MelhoresProdutoresRotas} />
             </Tab.Navigator>
