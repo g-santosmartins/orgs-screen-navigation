f3334c11016cb539bddd51d4a2a9471de74deb79
Guilherme Martins <g.santosmartins@outlook.com>
2022-05-21 22:44:46 -0300

    feat: Bucket params from route navigation

--- src/rotas/ProdutorRotas.js
+++ src/rotas/ProdutorRotas.js
@@ -3,14 +3,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Produtor from '../telas/Produtor'
 
 import Home from '../telas/Home';
+import Cesta from '../telas/Cesta';
 
 
 // calling the constructor of nav Stack component
 const Stack = createNativeStackNavigator()
 export default function ProdutorRotas({ComponentPrincipal = Home }) {
     return <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name='Home' component={ComponentPrincipal} />
+        <Stack.Screen name='HomeScreen' component={ComponentPrincipal} />
         <Stack.Screen name='Produtor' component={Produtor} />
+        <Stack.Screen name='Cesta' component={Cesta} />
+
 
     </Stack.Navigator>
 }
